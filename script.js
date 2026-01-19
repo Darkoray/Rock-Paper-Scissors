9828ad2f2113f3f98f8ab9d12bfefb07e5a24f89
Darkoray <soadulislam1234@gmail.com>
2026-01-19 22:30:09 +0600

    refactor: enhance game UI and improve button styles

--- script.js
+++ script.js
@@ -14,38 +14,57 @@ const moveBtns = document.querySelectorAll('.rock, .paper, .scissors');
 const resultEl = document.querySelector('.result');
 const resetBtn = document.querySelector('.reset');
 
+// Game loading or not
+let gameLoading = false;
+
 // Detects changes of move options
 moveBtns.forEach(btn => {
   btn.addEventListener('click', () => {
+    // Prevents spam clicks
+    if (gameLoading) return;
+    gameLoading = true;
+
     // Player and system moves and their indexes
     const systemMoveIndex = Math.floor(Math.random() * movesArr.length);
     const systemMove = movesArr.at(systemMoveIndex);
     const playerMove = btn.dataset.move;
     const playerMoveIndex = movesArr.indexOf(playerMove);
 
-    // Empty result
+    // Checks the result
     let result;
 
-    // Checks the result
     if (systemMove === playerMove) result = "It's a Draw";
     else if ((playerMoveIndex + 1) % movesArr.length === systemMoveIndex)
       result = 'You Lost ;-;';
     else result = 'You Won!!';
 
-    // Displays the result
+    // Shows loading animation
+    let dots = 1;
     resultEl.classList.remove('hidden');
-    const capitalize = str => str.at(0).toUpperCase() + str.slice(1);
-    resultEl.textContent = `System played ${capitalize(systemMove)}
-    ${movesArrValues.at(systemMoveIndex)}. ${result}`;
+    const loading = setInterval(() => {
+      dots = ++dots % 4;
+      resultEl.textContent = 'System is making a move' + '.'.repeat(dots);
+    }, 300);
 
-    // Shows reset button
-    resetBtn.classList.remove('hidden');
+    // Displays the result
+    setTimeout(() => {
+      clearInterval(loading);
+      const capitalize = str => str.at(0).toUpperCase() + str.slice(1);
+      resultEl.textContent = `System played ${capitalize(systemMove)}
+      ${movesArrValues.at(systemMoveIndex)}. ${result}`;
+
+      // Shows reset button
+      resetBtn.classList.remove('hidden');
+    }, 1500);
   });
 });
 
 // Resets everything
-resetBtn.addEventListener('click', () => {
-  resultEl.textContent = '';
-  resultEl.classList.add('hidden');
-  resetBtn.classList.add('hidden');
-});
+resetBtn.addEventListener('click', () =>
+  setTimeout(() => {
+    resultEl.textContent = '';
+    resultEl.classList.add('hidden');
+    resetBtn.classList.add('hidden');
+    gameLoading = false;
+  }, 150),
+);
